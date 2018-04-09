f653139a970b84b27c6e6df9aca7682a307fe064
Timosav <timotheesavoy@gmail.com>
2018-04-09 22:29:15 +0200

    Comments added

--- docx-reader.py
+++ docx-reader.py
@@ -1,6 +1,6 @@
-import os
+#import os
 from zipfile import ZipFile
-import bs4
+#import bs4
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup as BS
@@ -13,54 +13,64 @@ class docx_reader:
         self.doc = self._read_docx()
         
         summary = list()
+        
+        # find all the tables in the document for later indexing
         tables = self.doc.find_all('tbl')
         
-        # change the way to indicate if it's in table: detect if it's wrapped in a table
-        # and then add index based on the total length of the tables
-        # also add the cell idnex based on w:tblW w:type="dxa" w:w="8522"
-        # and w:gridCol w:w="4261"
-        
-        
-        tables = self.doc.find_all('tbl')
-        
+        # initial values for table/cell indexing
         current_table = 0
         current_cell = 0
         current_cell_index = [0,0]
         current_paragraph = 0
         current_table_width = int(tables[current_table].find('tblW').get('w:w'))
         current_table_nb_cells = len(tables[current_table].find_all('tc'))
-        current_nb_paragraph_in_cell = [len(x.find_all('p')) for x in tables[current_table].find_all('tc')]
+        current_nb_paragraph_in_cell = [len(x.find_all('p')) for x in \
+                                        tables[current_table].find_all('tc')]
                
         
         for i, paragraph in enumerate(self._iter_paragraphs()):
             index_table = None
             cell_index = None
             
+            # if we have a table as parent, then ...
             if paragraph.find_parent('tbl'):
-                current_paragraph += 1
+                # values to be outputed
                 cell_index = "_".join([str(int(x)) for x in current_cell_index])
                 index_table = str(current_table + 1)
-#                print(index_table)
-                add_width = int(paragraph.find_parent('tc').find('tcW').get('w:w'))/current_table_width
-                if int(current_cell_index[0]) != int(current_cell_index[0] + add_width):
-                    current_cell_index[1] = 0
-                else :
-                    current_cell_index[1] += 1
-                current_cell_index[0] += add_width
+                
+                # we are looking at a new paragraph inside the cell
+                current_paragraph += 1
+                                
+                # we now have to define if the new cell finished, if so we 
+                # add its width/table_width to see if we start a new row or not
                 if current_paragraph == current_nb_paragraph_in_cell[current_cell]:
+                    add_width = int(paragraph.find_parent('tc').find('tcW')\
+                                    .get('w:w'))/current_table_width
+                    if int(current_cell_index[0]) !=\
+                                int(current_cell_index[0] + add_width):
+                        current_cell_index[1] = 0
+                    else :
+                        current_cell_index[1] += 1
+                    current_cell_index[0] += add_width
+                    
+                    # then we increase the cell count and reset current paragraph
                     current_cell += 1
                     current_paragraph = 0
+                    # if we reached the end of the table, we now select the next
+                    # table
                     if current_cell == current_table_nb_cells:
                         current_table += 1
                         if current_table == len(tables):
                             pass
                         else:
                             current_cell = 0
-                            current_table_width = int(tables[current_table].find('tblW').get('w:w'))
-                            current_table_nb_cells = len(tables[current_table].find_all('tc'))
-                            current_nb_paragraph_in_cell = [len(x.find_all('p')) for x in tables[current_table].find_all('tc')]
+                            current_table_width = int(tables[current_table]\
+                                                      .find('tblW').get('w:w'))
+                            current_table_nb_cells = len(tables[current_table]\
+                                                         .find_all('tc'))
+                            current_nb_paragraph_in_cell = [len(x.find_all('p'))\
+                                                            for x in tables[current_table].find_all('tc')]
                             current_cell_index = [0,0]
-                
             
             text = paragraph.text
     
@@ -70,7 +80,10 @@ class docx_reader:
             list_lvl = self._try_or_none_properties(properties, 'w:ilvl', 'w:val')
             list_type = self._try_or_none_properties(properties, 'w:numId', 'w:val')
             position = self._try_or_none_properties(properties, 'jc', 'w:val')
+            bookmark = self._try_or_none_properties(paragraph, 'bookmarkStart', 'w:id')
             
+            # We analyze the properties of each run : if runs in the same 
+            # paragraph have different style, it appears as a list in the pandas
             runs_prop = []
             run_prop = [ 'vertAlign', 'b', 'i', 'color', 'highlight', 'u', 'bCs', 'iCs']
             try:
@@ -89,12 +102,6 @@ class docx_reader:
 #                    tabs_prop.append([i.get('w:pos'), i.get('w:val')])
 #            except AttributeError:
 #                pass
-            
-            bookmark = self._try_or_none_properties(paragraph, 'bookmarkStart', 'w:id')
-            
-#            for i, table in enumerate(tables):
-#                if paragraph in table.find_all('p'):
-#                    index_table = 'table_' + str(i)
             
             if paragraph.find('cNvPr'):
                 img_id = "_".join(['image', paragraph.find('cNvPr').get('id')])
